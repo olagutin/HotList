9d55654a96e834d24e40b93679a53e8a0e460ca0
Oleg Lagutin <olagutin11@gmail.com>
2024-02-21 23:18:12 +0300

    conditionServiceUpdate

--- src/main/java/com/hotlist/service/ConditionService.java
+++ src/main/java/com/hotlist/service/ConditionService.java
@@ -1,10 +1,15 @@
 package com.hotlist.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
+@Slf4j
 @Service
 public class ConditionService {
     private final WebClient webClient;
@@ -13,13 +18,23 @@ public class ConditionService {
     private String externalUrl;
 
     public ConditionService(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl(externalUrl).build();
+        var httpClient = HttpClient.newConnection().keepAlive(false);
+        var reactorClientHttpConnector = new ReactorClientHttpConnector(httpClient);
+        this.webClient = webClientBuilder.clientConnector(reactorClientHttpConnector).build();
+        LOGGER.info("webClient is built");
     }
 
     @Scheduled(cron = "0 0/14 * * * ?")
-    public void checkAvailability() {
-        System.out.println(webClient.get().uri("/condition")
-                .retrieve().toEntity(String.class)
-                .block());
+    public void checkAvailability() throws InterruptedException {
+        LOGGER.info(getDetails().toString());
+    }
+
+    public ResponseEntity<String> getDetails() {
+        return webClient
+                .get()
+                .uri(externalUrl + "/condition")
+                .retrieve()
+                .toEntity(String.class)
+                .block();
     }
 }
